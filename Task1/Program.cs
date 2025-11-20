3ab8e8afd0f67d8608746e4ed91d7e505f8d9638
NirjalaGhimire <nirjalaghimire012@gmail.com>
2025-11-20 08:10:19 +0545

    Task1 completed

---
+++ Task1/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s1 = new Student();
+            Student s2 = new Student();
+
+            s1.name = "Nisha";
+            s1.age = 24;
+            s1.grade = 15;
+
+            s2.name = "Khushi";
+            s2.age = 25;
+            s1.grade = 5;
+ 
+            Console.WriteLine("----- Student 1 -------");
+            Console.WriteLine("Name: " + s1.name);
+            Console.WriteLine("age: " + s1.age);
+            Console.WriteLine("grade: " + s1.grade);
+
+            Console.WriteLine("----- Student 2 -------");
+            Console.WriteLine("Name: " + s2.name);
+            Console.WriteLine("Age: " + s2.age);
+            Console.WriteLine("Course: " + s2.grade);
+
+            Console.WriteLine("\nCollege Name: " + Student.collegeName);
+        }
+    }
+}
---
+++ Task1/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    internal class Student
+    {
+        public string name;
+        public int age;
+        public int grade;
+
+        public static string collegeName = "Itahari International college";
+
+    }
+}
